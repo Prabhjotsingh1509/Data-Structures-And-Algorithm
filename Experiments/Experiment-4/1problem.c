be758fd5381e4b6d95c18ac79a47c231e1df5ce9
Prabhjot Singh <amssre.16461@gmail.com>
2026-03-26 21:37:29 +0530

    Queue operations

---
+++ Experiments/Experiment-4/1problem.c
@@ -0,0 +1,64 @@
+// Wap to insert element in queue using static memory
+#include <stdio.h>
+#define MAX 5
+
+int queue[MAX];
+int front = -1;
+int rear = -1;
+
+void enqueue(int data)
+{
+    if (rear == MAX - 1)
+    {
+        printf("Queue Overflow\n");
+        return;
+    }
+    if (front == -1)
+        front = 0;
+
+    rear++;
+    queue[rear] = data;
+
+    printf("Inserted: %d\n", data);
+}
+
+void display()
+{
+    if (front == -1)
+    {
+        printf("Queue is Empty\n");
+        return;
+    }
+
+    for (int i = front; i <= rear; i++)
+    {
+        printf("%d ", queue[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int choice, data;
+
+    while (1)
+    {
+        printf("\n1. Insert\n2. Display\n3. Exit\n");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            scanf("%d", &data);
+            enqueue(data);
+            break;
+
+        case 2:
+            display();
+            break;
+
+        case 3:
+            return 0;
+        }
+    }
+}
---
+++ Experiments/Experiment-4/2problem.c
@@ -0,0 +1,75 @@
+// Wap to insert element in queue using linkedlist
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node *front = NULL;
+struct Node *rear = NULL;
+
+void enqueue(int data)
+{
+    struct Node *newnode = (struct Node *)malloc(sizeof(struct Node));
+    newnode->data = data;
+    newnode->next = NULL;
+
+    // If queue is empty
+    if (front == NULL)
+    {
+        front = rear = newnode;
+    }
+    else
+    {
+        rear->next = newnode;
+        rear = newnode;
+    }
+
+    printf("Inserted: %d\n", data);
+}
+
+void display()
+{
+    if (front == NULL)
+    {
+        printf("Queue is Empty\n");
+        return;
+    }
+
+    struct Node *temp = front;
+    while (temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int choice, data;
+
+    while (1)
+    {
+        printf("\n1. Enqueue\n2. Display\n3. Exit\n");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            scanf("%d", &data);
+            enqueue(data);
+            break;
+
+        case 2:
+            display();
+            break;
+
+        case 3:
+            return 0;
+        }
+    }
+}
