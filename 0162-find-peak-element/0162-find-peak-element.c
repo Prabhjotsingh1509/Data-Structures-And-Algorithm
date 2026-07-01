22a83a92afb50f6fa5296d4138a893acd3bdf289
Prabhjot Singh <amssre.16461@gmail.com>
2026-07-01 22:47:27 +0530

    Time: 0 ms (100%), Space: 8.7 MB (91.37%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.c
+++ 0162-find-peak-element/0162-find-peak-element.c
@@ -5,16 +5,13 @@ int findPeakElement(int* nums, int numsSize) {
     while (low < high) {
         int mid = low + (high - low) / 2;
         
-        // If the next element is greater, the peak is on the right side
         if (nums[mid] < nums[mid + 1]) {
             low = mid + 1;
         } 
-        // Otherwise, the peak is either at 'mid' or to the left
         else {
             high = mid;
         }
     }
     
-    // 'low' and 'high' will converge to the peak element index
     return low;
 }
