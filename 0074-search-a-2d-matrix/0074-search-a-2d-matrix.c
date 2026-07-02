7b477ab451a788141c71e8e8b77651c77d75fc49
Prabhjot Singh <amssre.16461@gmail.com>
2026-07-02 15:45:35 +0530

    Time: 0 ms (100%), Space: 9.6 MB (54.58%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.c
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.c
@@ -1,14 +1,40 @@
 bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target) {
     
-    for(int i=0;i<matrixSize;i++)
+    // for(int i=0;i<matrixSize;i++)
+    // {
+    //     for(int j=0;j<matrixColSize[0];j++)
+    //     {
+    //         if(matrix[i][j] == target)
+    //         {
+    //             return true;
+    //         }
+    //     }
+    // }
+    // return false;
+    int rows=matrixSize;
+    int cols=matrixColSize[0];
+    int low=0;
+    int high= (rows*cols)-1;
+
+    while(low <= high)
     {
-        for(int j=0;j<matrixColSize[0];j++)
+        int mid=low+(high-low)/2;
+        int row = mid/cols;
+        int col = mid%cols;
+
+        if(matrix[row][col]==target)
         {
-            if(matrix[i][j] == target)
-            {
-                return true;
-            }
+            return true;
+        }
+        else if(matrix[row][col]<target)
+        {
+            low=mid+1;
+        }
+        else
+        {
+            high=mid-1;
         }
     }
     return false;
+
 }
