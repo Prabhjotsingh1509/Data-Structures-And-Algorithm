d3af45b0e4c232255f3e9c77dfbdecebd59edeac
Prabhjot Singh <amssre.16461@gmail.com>
2026-07-14 02:00:13 +0530

    Time: 0 ms (100%), Space: 10.3 MB (43.03%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.c
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.c
@@ -6,28 +6,48 @@
  * };
  */
 struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
-    struct ListNode* temp= head;
-    int count=0;
-    while(temp!= NULL)
+    // struct ListNode* temp= head;
+    // int count=0;
+    // while(temp!= NULL)
+    // {
+    //     count++;
+    //     temp=temp->next;
+    // }
+    // int pos = count - n;
+    // if(pos == 0)
+    // {
+    //     head=head->next;
+    //     return head;
+    // }
+    // struct ListNode *prev=NULL;
+    // temp=head;
+    // for(int i=0;i<pos;i++)
+    // {
+    //     prev=temp;
+    //     temp=temp->next;
+    // }
+    // prev->next=temp->next;
+
+    // free(temp);
+    // return head;
+    struct ListNode *dummy= malloc(sizeof(struct ListNode));
+    dummy->next= head;
+
+    struct ListNode* slow= dummy;
+    struct ListNode* fast=dummy;
+
+    for(int i=0;i<=n;i++)
     {
-        count++;
-        temp=temp->next;
+        fast = fast->next;
     }
-    int pos = count - n;
-    if(pos == 0)
+    while(fast != NULL)
     {
-        head=head->next;
-        return head;
+        fast = fast->next;
+        slow=slow->next;
     }
-    struct ListNode *prev=NULL;
-    temp=head;
-    for(int i=0;i<pos;i++)
-    {
-        prev=temp;
-        temp=temp->next;
-    }
-    prev->next=temp->next;
+    struct ListNode * temp = slow->next;
+    slow->next= temp->next;
 
     free(temp);
-    return head;
+    return dummy->next;
 }
